305d935732cc0227fa486acb24ac329785ec39fd
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-07-26 16:07:58 +0500

    Challenge 1 Variables Practice Solved

--- Challenge 1 Variables Practice/script.js
+++ Challenge 1 Variables Practice/script.js
@@ -3,3 +3,10 @@
 // Concatenate the two variables into a third variable called fullName
 
 // Log fullName to the console
+
+let firstName = "Syed ";
+let lastName = "Muzammil";
+
+let fullName = firstName + lastName;
+
+console.log(fullName);
