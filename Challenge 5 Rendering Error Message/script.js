28e48d832b81f8ea5ed29b51f395f6ca237f32e2
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-07-26 16:27:38 +0500

    Challenge 5 Solved

--- Challenge 5 Rendering Error Message/script.js
+++ Challenge 5 Rendering Error Message/script.js
@@ -1,3 +1,9 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
+
+let error = document.getElementById("error");
+
+function errorMessage() {
+  error.textContent = "Something went wrong, please try again";
+}
