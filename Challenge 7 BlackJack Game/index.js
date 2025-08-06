44317d3a1242ec7ee38b35d104cb80e3529dbb40
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-06 18:32:29 +0500

    Add eligibility check for King's birthday card
    Commented out previous Blackjack and nightclub age check logic. Added new logic to check if a person is eligible for a birthday card from the King based on age.

--- Challenge 7 BlackJack Game/index.js
+++ Challenge 7 BlackJack Game/index.js
@@ -1,34 +1,50 @@
 // 1. Create two variables, firstCard and secondCard.
-let firstCard = 7;
+// let firstCard = 7;
 
 // Set their values to a random number between 2-11
-let secondCard = 13;
+// let secondCard = 13;
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
-let sum = firstCard + secondCard;
+// let sum = firstCard + secondCard;
 
-console.log(sum);
+// console.log(sum);
 
-if (sum === 21) {
-  console.log(`You Win 🥳. You've got Blackjack!`);
-} else if (sum < 21) {
-  console.log(
-    `You are still in the game 🙃. Do you want to draw a new card? 🙂`
-  );
-} else {
-  console.log(`You lost badly 🥴`);
-}
+// if (sum === 21) {
+//   console.log(`You Win 🥳. You've got Blackjack!`);
+// } else if (sum < 21) {
+//   console.log(
+//     `You are still in the game 🙃. Do you want to draw a new card? 🙂`
+//   );
+// } else {
+//   console.log(`You lost badly 🥴`);
+// }
 
 // Check if the person is old enough to enter the nightclub (21)
 // Log a suitable message to the console in both cases
 
-let age = 23;
+// let age = 23;
 
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
 
-if (age < 21) {
-  console.log(`You can not enter the club!`);
+// if (age < 21) {
+//   console.log(`You can not enter the club!`);
+// } else {
+//   console.log(`Welcome`);
+// }
+
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age = 100;
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (age < 100) {
+  console.log(`Not elegible`);
+} else if (age === 100) {
+  console.log(`Here is your birthday card from the King!`);
 } else {
-  console.log(`Welcome`);
+  console.log(`Not elegible, you have already gotten one`);
 }
