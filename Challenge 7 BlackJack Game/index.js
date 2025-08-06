455d2b10509f507b2b22c0c8f3faf5775e565b8f
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-06 18:11:54 +0500

    Add basic Blackjack game logic and output
    Implemented logic to check the sum of two cards and print messages for win, continue, or loss scenarios in the Blackjack game.

--- Challenge 7 BlackJack Game/index.js
+++ Challenge 7 BlackJack Game/index.js
@@ -6,3 +6,15 @@ let secondCard = 13;
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard;
+
+console.log(sum);
+
+if (sum === 21) {
+  console.log(`You Win 🥳. You've got Blackjack!`);
+} else if (sum < 21) {
+  console.log(
+    `You are still in the game 🙃. Do you want to draw a new card? 🙂`
+  );
+} else {
+  console.log(`You lost badly 🥴`);
+}
