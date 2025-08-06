ba3091072b69f71ca15d643de888d59e37d76c27
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-06 18:19:45 +0500

    Add age check for nightclub entry
    Introduced logic to check if a person is old enough (21+) to enter the nightclub and log an appropriate message based on their age.

--- Challenge 7 BlackJack Game/index.js
+++ Challenge 7 BlackJack Game/index.js
@@ -18,3 +18,17 @@ if (sum === 21) {
 } else {
   console.log(`You lost badly 🥴`);
 }
+
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 23;
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age < 21) {
+  console.log(`You can not enter the club!`);
+} else {
+  console.log(`Welcome`);
+}
