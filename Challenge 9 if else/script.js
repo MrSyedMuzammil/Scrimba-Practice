91293e9a70a69b95bc3fed9523d32be0a733dead
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-16 14:22:04 +0500

    Initial Commit Add Challenge 9 if else HTML and JS files
    Introduces a new challenge with index.html and script.js for practicing if/else statements based on age-related discounts.

---
+++ Challenge 9 if else/script.js
@@ -0,0 +1,12 @@
+// Practice 2 Challenge 2
+
+let age = 75;
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
