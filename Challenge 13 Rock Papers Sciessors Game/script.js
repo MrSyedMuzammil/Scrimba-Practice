a011b78a26a0d29a31c3047558a98355975047fd
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-16 15:00:44 +0500

    Add random hand selection function to game
    Introduced a 'game' function that returns a random item from the 'hands' array and logs the result. This implements the random choice logic for the Rock Paper Scissors game.

--- Challenge 13 Rock Papers Sciessors Game/script.js
+++ Challenge 13 Rock Papers Sciessors Game/script.js
@@ -1,3 +1,13 @@
 let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
+
+// Practice 2 Challenge 5
+
+// Solution :
+
+function game() {
+  return hands[Math.floor(Math.random() * hands.length)];
+}
+
+console.log(game());
