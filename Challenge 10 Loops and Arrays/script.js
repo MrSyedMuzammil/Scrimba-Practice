2ff12cd307f4fe202fbae7f4dca02dffc7de4a3c
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-16 14:27:10 +0500

    Initial Commit Add Challenge 10 Loops and Arrays files
    Created index.html and script.js for Challenge 10, setting up the structure for practicing loops and arrays. Includes initial array of large countries and instructions for logging them using a for loop.

---
+++ Challenge 10 Loops and Arrays/script.js
@@ -0,0 +1,13 @@
+// Practice 2 Challenge 3
+
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
