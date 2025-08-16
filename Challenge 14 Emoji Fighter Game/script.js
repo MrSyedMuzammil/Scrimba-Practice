4ffa99a204d1b46109ffbef55cdb9cf04762b48f
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-16 15:34:53 +0500

    Implement random emoji fighter selection
    Replaces the placeholder console log with logic to pick two random emoji fighters and display them in the stage element when the button is clicked.

--- Challenge 14 Emoji Fighter Game/script.js
+++ Challenge 14 Emoji Fighter Game/script.js
@@ -25,5 +25,8 @@ fightButton.addEventListener("click", function () {
   // Challenge:
   // When the user clicks on the "Pick Fighters" button, pick two random
   // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
-  console.log("Game Started Here");
+  let firstFighter = fighters[Math.floor(Math.random() * fighters.length)];
+  let secondFighter = fighters[Math.floor(Math.random() * fighters.length)];
+  console.log(`${firstFighter} vs ${secondFighter}`);
+  stageEl.innerHTML = `${firstFighter} vs ${secondFighter}`;
 });
