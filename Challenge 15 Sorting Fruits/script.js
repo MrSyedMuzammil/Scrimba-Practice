5b4b6b910d0452ffc0ad8593de419eef70c3ba01
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-18 10:45:41 +0500

    The Loop inside of the Function to  sort the fruit
    Previously, we just used loop without a function. Now we enclosed that inside of a function, when the function is called it sorts the fruit in its shelf.

--- Challenge 15 Sorting Fruits/script.js
+++ Challenge 15 Sorting Fruits/script.js
@@ -6,10 +6,13 @@ let orangeShelf = document.getElementById("orange-shelf");
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
 
-for (let i = 0; i < fruit.length; i++) {
-  if (fruit[i] === "🍎") {
-    appleShelf.textContent += "🍎";
-  } else {
-    orangeShelf.textContent += "🍊";
+function sortFruit() {
+  for (let i = 0; i < fruit.length; i++) {
+    if (fruit[i] === "🍎") {
+      appleShelf.textContent += "🍎";
+    } else {
+      orangeShelf.textContent += "🍊";
+    }
   }
 }
+sortFruit();
