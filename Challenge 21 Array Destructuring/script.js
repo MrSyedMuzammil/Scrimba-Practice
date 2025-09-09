db584956ac009db22111044d34bdc62a4a077a76
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-09-09 17:38:23 +0500

    feat(challenge-21): Implement solution for array destructuring
    This commit provides the solution for the "Challenge 21: Array Destructuring" exercise.
    
    It uses an ES6 array destructuring assignment to unpack the elements from the `numbers` array into `first`, `second`, and `third` variables. Console log statements are included to verify that the elements were extracted correctly.

--- Challenge 21 Array Destructuring/script.js
+++ Challenge 21 Array Destructuring/script.js
@@ -1,3 +1,8 @@
 const numbers = [10, 20, 30];
 
-// Destructure the array
+// Destructuring the array
+const [first, second, third] = numbers;
+
+console.log(`This is the first number : ${first}`);
+console.log(`This is the second number : ${second}`);
+console.log(`This is the third number : ${third}`);
