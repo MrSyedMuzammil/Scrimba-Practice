d4d4aaec221e883c1e0263b98b01fc937960dc37
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-09-09 18:02:19 +0500

    refactor(challenge-22): Use parameter destructuring in addressMaker
    This commit refactors the `addressMaker` function to be more concise and declarative by using ES6 features.
    
    The function signature now uses parameter destructuring to directly extract properties from the input object. This eliminates the redundant intermediate variable and makes the code easier to read and maintain.

--- Challenge 22 Object Literals/script.js
+++ Challenge 22 Object Literals/script.js
@@ -1,11 +1,26 @@
+// function addressMaker(address) {
+//   const newAddress = {
+//     city: address.city,
+//     state: address.state,
+//     country: "Pakistan",
+//   };
+// }
+
+// addressMaker({ city: "Peshawar", state: "KPK" });
+
+// Refactor Code to use Object Destructuring and Template Literals
+
 function addressMaker(address) {
   const newAddress = {
     city: address.city,
     state: address.state,
     country: "Pakistan",
   };
+  const { city, state, country } = newAddress;
+
+  console.log(`This is the city: ${city}`);
+  console.log(`This is the state: ${state}`);
+  console.log(`This is the country: ${country}`);
 }
 
-// addressMaker({ city: "Peshawar", state: "KPK" });
-
-// Refactor Code to use Object Destructuring and
+addressMaker({ city: "Peshawar", state: "KPK" });
