9ca3f5b6a1b8b2818740b7f5bced7ca7d76a492f
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-09-09 17:53:06 +0500

    This commit introduces the initial files for the Challenge 22 exercise.  It includes: - `index.html`: A standard HTML boilerplate to run the script. - `script.js`: A placeholder file for the upcoming challenge logic.
    This commit introduces the initial files for the Challenge 22 exercise.
    
    It includes:
    - `index.html`: A standard HTML boilerplate to run the script.
    - `script.js`: A placeholder file for the upcoming challenge logic.

---
+++ Challenge 22 Object Literals/script.js
@@ -0,0 +1,11 @@
+function addressMaker(address) {
+  const newAddress = {
+    city: address.city,
+    state: address.state,
+    country: "Pakistan",
+  };
+}
+
+// addressMaker({ city: "Peshawar", state: "KPK" });
+
+// Refactor Code to use Object Destructuring and
